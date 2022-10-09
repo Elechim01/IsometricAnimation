a17bffa93f47684939b9ff97e3d85f83b543374c
Elechim01 <68447158+Elechim01@users.noreply.github.com>
2022-10-09 19:10:38 +0200

    Initial Commit

---
+++ IsometricAnimation/IsometricAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  IsometricAnimationApp.swift
+//  IsometricAnimation
+//
+//  Created by Michele Manniello on 09/10/22.
+//
+
+import SwiftUI
+
+@main
+struct IsometricAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
